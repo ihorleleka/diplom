2fff18adb34eb28503168a7bd99b7f50c32265a6
Ihor Leleka <ihor.leleka@valtech.com>
2017-05-09 17:36:29 +0300

    update FE feedBack

--- UIpart/ui/src/js/app/shared/feedBack.js
+++ UIpart/ui/src/js/app/shared/feedBack.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import validation from './validation.js';
+import PubSub from 'pubsub-js';
 
 var feedBack = (function () {
   'use strict';
@@ -7,13 +8,17 @@ var feedBack = (function () {
   var feedBack = {
     init: function () {
       feedBack.bindEvents();
+      PubSub.subscribe('feedbackSent', feedBack.feedbackSent);
+      PubSub.subscribe('feedbackSendFail', feedBack.feedbackSendFail);
     },
 
     bindEvents: function () {
       $('.feedbackPage button.send').click(function (event) {
         var pageForFeedBack = $(this).parents('.page');
-        if (validation.validatePage(pageForFeedBack)) {
-          feedBack.collectData(pageForFeedBack);
+        if (validation.validatePage(pageForFeedBack) && !$(this).hasClass('clicked')) {
+          $(this).addClass('clicked');
+          var data = feedBack.collectData(pageForFeedBack);
+          PubSub.publishSync('SendFeedback', data);
         }
       });
     },
@@ -23,7 +28,20 @@ var feedBack = (function () {
       collectedData.email = page.find('.email').val();
       collectedData.name = page.find('.name').val();
       collectedData.comment = page.find('.comment').val();
-      console.log(collectedData);
+      return collectedData;
+    },
+
+    feedbackSent: function (msg, data) {
+      var $feedbackPageBottom = $('.feedbackPage .row .col-xs-12 .buttons-block');
+      $feedbackPageBottom.find('p').remove();
+      $feedbackPageBottom.append('<p>Дякуємо за Ваше повідомлення!</p>');
+    },
+
+    feedbackSendFail: function (msg, data) {
+      var $feedbackPageBottom = $('.feedbackPage .row .col-xs-12 .buttons-block');
+      $feedbackPageBottom.find('p').remove();
+      $feedbackPageBottom.append(`<p>${data.xhr.responseText}</p>`);
+      $('.regPage button.reg').removeClass('clicked');
     }
   };
 
--- UIpart/ui/src/js/app/shared/requestSender.js
+++ UIpart/ui/src/js/app/shared/requestSender.js
@@ -12,6 +12,7 @@ var requestSender = (function () {
 
     bindEvents: function () {
       PubSub.subscribe('RegistrationAttempt', requestSender.registrationHandler);
+      PubSub.subscribe('SendFeedback', requestSender.sendFeedback);
     },
 
     registrationHandler: function (msg, data) {
@@ -21,6 +22,16 @@ var requestSender = (function () {
       })
       .fail(function (xhr, status, error) {
         PubSub.publishSync('registrationFail', { xhr: xhr, status: status, error: error });
+      });
+    },
+
+    sendFeedback: function (msg, data) {
+      AjaxService.post('feedback.php', data)
+      .done(function (msg) {
+        PubSub.publishSync('feedbackSent', msg);
+      })
+      .fail(function (xhr, status, error) {
+        PubSub.publishSync('feedbackSendFail', { xhr: xhr, status: status, error: error });
       });
     }
   };
