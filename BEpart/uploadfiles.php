385ba052203e26da8ec4dd9c5e3b76ab081ecc07
Ihor Leleka <ihor.leleka@valtech.com>
2017-06-15 19:05:51 +0300

    put downloaded files in files folder

--- BEpart/uploadfiles.php
+++ BEpart/uploadfiles.php
@@ -4,13 +4,13 @@ if(isset( $_GET['uploadfiles'])) {
     $error = false;
     $files = array();
 
-    $uploaddir = './uploads/'; 
+    $uploaddir = '../files/'; 
     if( ! is_dir( $uploaddir ) ) mkdir( $uploaddir, 0777 );
  
     foreach( $_FILES as $file ){
         $_newname = $file['name'];
         if( move_uploaded_file( $file['tmp_name'], $uploaddir . basename($file['name']))){           
-            $files[] = 'php/uploads/' . $_newname;
+            $files[] = 'files/' . $_newname;
         }
         else{
             $error = true;
